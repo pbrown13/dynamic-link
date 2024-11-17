42fa5b13c20a515e1cb08488a57d8bf737a33f9c
parker@jbweld.com <parker@jbweld.com>
2024-11-17 17:39:24 -0500

    123

---
+++ index.js
@@ -0,0 +1,42 @@
+(function () {
+  // Retrieve SKU dynamically from the page
+  const skuElement = document.querySelector('span[itemprop="sku"]');
+  const sku = skuElement ? skuElement.textContent : null;
+  if (!sku) {
+    console.error("SKU element not found. Cannot generate iframe link.");
+    return;
+  }
+
+  const refUrl = encodeURIComponent(window.location.href);
+
+  function generateUserId() {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+      /[xy]/g,
+      function (c) {
+        const r = (Math.random() * 16) | 0,
+          v = c === "x" ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      }
+    );
+  }
+
+  let userId = localStorage.getItem("userId");
+  if (!userId) {
+    userId = generateUserId();
+    localStorage.setItem("userId", userId);
+  }
+
+  window.gtag("get", "G-DD9K9M9485", "session_id", function (sessionId) {
+    if (sessionId) {
+      const iframeLink = `https://web.jbweld.com/mapview?sku=${sku}&session_id=${sessionId}&user_id=${userId}&ref_url=${refUrl}`;
+      const button = document.querySelector("#findRetailerButton");
+      if (button) {
+        button.href = iframeLink;
+      } else {
+        console.error("Button with ID 'findRetailerButton' not found.");
+      }
+    } else {
+      console.error("Session ID could not be retrieved.");
+    }
+  });
+})();
